2b0b0f4893f76463cb327f20f8cfa45a525fc7de
Bonnie Schulkin <bonnie@learnersguild.com>
2017-12-18 13:57:33 -0800

    Add solution for challenge #8

--- pets/public/js/pets.js
+++ pets/public/js/pets.js
@@ -17,3 +17,20 @@ speciesCells.forEach((cell) => {
     event.target.style.backgroundColor = '#fff'
   })
 })
+
+const pageChangeBtn = document.querySelector('#change-title')
+const pageTitle = document.querySelector('#page-title')
+
+pageChangeBtn.addEventListener('click', (event) => {
+  if (pageTitle.innerHTML === 'Pets') {
+    pageTitle.innerHTML = 'Super Pets'
+    event.target.innerHTML = 'Change title back'
+  }
+  else if (pageTitle.innerHTML === 'Super Pets') {
+    pageTitle.innerHTML = 'Pets';
+    event.target.innerHTML = 'Change Title'
+  }
+  else {
+    console.error('I don\'t know what you clicked, but it wasn\'t what I expected')
+  }
+})
